c07a5301bac7d98231ffff0613bb15bb02bdd635
AreYouScorpio <akos.schweger@gmail.com>
2021-11-23 14:43:39 +0100

    test human task

--- src/test/java/day01/HumanTest.java
+++ src/test/java/day01/HumanTest.java
@@ -12,6 +12,22 @@ public class HumanTest {
         // Human h = new Human(1900, "John Doe");
         assertThrows(IllegalArgumentException.class,()-> new Human(1900, "John Doe"));
     }
+/*
+    @Test
+    void HumanTestOK(){
+        Human human = new Human(1976, "John Doe");
+        assertEquals("John Doe", human.getName());
+        assertEquals(1976, human.getYearOfBirth());
+    }
+*/
+
+    /*
+    @Test
+    void HumanTestNOK(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()-> new Human(1976, "John Doe")); //elég a new-tól, m már a létrehozás hibát dob
+        assertEquals("We need both surename and firstname.", exception.getMessage());
+    }
+     */
 
 }
 
