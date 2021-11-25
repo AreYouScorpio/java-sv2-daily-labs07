7d6cd265b58002723eeedabd69b6d4d39204b860
AreYouScorpio <akos.schweger@gmail.com>
2021-11-25 23:10:04 +0100

    day04 fa gyakorlas done all test

--- src/main/java/day04/Courier.java
+++ src/main/java/day04/Courier.java
@@ -7,7 +7,8 @@ public class Courier {
     public static List<Ride> rides = new ArrayList<>();
 
     public static void addRide(Ride newRide) {
-        if (newRide.getDay() > rides.get(rides.size() - 1).getDay() ) {
+        if (rides.size()==0) {rides.add(newRide);}
+        else if (newRide.getDay() > rides.get(rides.size() - 1).getDay() ) {
             rides.add(newRide);
         }
         else if (newRide.getDay() == rides.get(rides.size() - 1).getDay() && newRide.getRideNumberThisDay()>rides.get(rides.size()-1).getRideNumberThisDay()) {
@@ -16,6 +17,8 @@ public class Courier {
         else {
             throw new IllegalArgumentException("Nem vihető fel utólag korábbi adat.");
         } ;
+
+        //új nap mindig első ride-dal kezdődjön, ezt is checkolni kellene
 
     }
 
